b8a758da47e49b6c1243119371eb6b7213500b67
Faye Ellis <36965558+fayecloudguru@users.noreply.github.com>
2021-04-23 11:41:39 +0100

    Create roleconfig.sh

---
+++ Automation_With_AWS_Config_Demo/roleconfig.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+echo "creating IAM Role 'MyAutomationRole' for auto remediation rule"
+iamRoleArn=$(aws iam create-role --role-name "MyAutomationRole" \
+--assume-role-policy-document '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": [
+                    "ec2.amazonaws.com",
+                    "ssm.amazonaws.com"
+                ]
+            },
+            "Action": [
+                "sts:AssumeRole"
+            ]
+        }
+    ]
+}' --query "Role.Arn" --output=text)
+aws iam attach-role-policy --role-name "MyAutomationRole" \
+ --policy-arn "arn:aws:iam::aws:policy/service-role/AmazonSSMAutomationRole"
+aws iam put-role-policy --role-name "MyAutomationRole" \
+ --policy-name “AllowPassRole” \
+ --policy-document '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "iam:PassRole",
+            "Resource": "'$iamRoleArn'"
+        }
+    ]
+}'
