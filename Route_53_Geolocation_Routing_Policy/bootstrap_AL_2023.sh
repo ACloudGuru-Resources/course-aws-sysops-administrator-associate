4d645e2553b90b5abba871918724f290376cd8dc
Faye Ellis <36965558+fayecloudguru@users.noreply.github.com>
2023-07-10 18:36:36 +0100

    Add files via upload

---
+++ Route_53_Geolocation_Routing_Policy/bootstrap_AL_2023.sh
@@ -0,0 +1,6 @@
+#!/bin/bash	
+dnf update -y
+dnf install httpd -y
+echo "<html><body><h1>Hello Cloud Gurus - This web page is being served from EC2.</h1></body></html>" >/var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
