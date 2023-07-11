151dc889a59a48990b1e7e669c15e0281f2ca091
Faye Ellis <36965558+fayecloudguru@users.noreply.github.com>
2023-07-11 12:49:13 +0100

    Update bootstrap_AL-2023.sh

--- Deploying_an_EC2_Instance_Demo/bootstrap_AL-2023.sh
+++ Deploying_an_EC2_Instance_Demo/bootstrap_AL-2023.sh
@@ -2,5 +2,5 @@
 dnf update -y
 dnf install httpd -y
 echo "<html><body><h1>Hello Cloud Gurus</h1></body></html>" >/var/www/html/index.html
-systemctl enable httpd
 systemctl start httpd
+systemctl enable httpd
